af15f391bde2d89e152b4e3f2a510fef7a03de2e
messenio <alias.tdp@gmail.com>
2017-12-05 20:41:04 -0500

    Added message for add track button

--- app/src/main/java/com/uie/musictrak/activities/activities/trackedit/TrackEditor.java
+++ app/src/main/java/com/uie/musictrak/activities/activities/trackedit/TrackEditor.java
@@ -1,14 +1,18 @@
 package com.uie.musictrak.activities.activities.trackedit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.uie.musictrak.R;
+import com.uie.musictrak.activities.activities.songview.YourSongs;
 
 public class TrackEditor extends AppCompatActivity {
 
@@ -41,6 +45,13 @@ public class TrackEditor extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        TextView add_track_button = findViewById(R.id.add_track_button);
+        add_track_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(findViewById(R.id.track_edit_body), "In a real app, this would open the community editor to find tracks", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
-
 }
